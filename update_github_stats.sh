b93ffa1bf3b6b6d25a0f467538a7515d6ccde714
Roan Rothrock <roan.rothrock@hotmail.com>
2023-03-18 10:09:50 -0500

    Update update_github_stats.sh

--- update_github_stats.sh
+++ update_github_stats.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# just like the other one, but for the stat API
 
 update_apis_github(){
   git init
   git pull
 
-  rm -f "activity_stats.svg"
+  rm "activity_stats.svg"
   curl "https://github-readme-stats.vercel.app/api?username=R-Rothrock&show_icons=true&theme=dark" > "activity_stats.svg"
   git add "activity_stats.svg"
 
-  rm -f "language_stats.svg"
-  curl "https://github-readme-stats-sigma-five.vercel.app/api/top-langs/?username=R-Rothrock&theme=react&line_height=40&hide=makefile,javascript,html&layout=compact" > "language_stats.svg"
+  rm "language_stats.svg"
+  curl "https://github-readme-stats-sigma-five.vercel.app/api/top-langs/?username=R-Rothrock&theme=react" > "language_stats.svg"
   git add "language_stats.svg"
 
   git commit -m "Updated \`activity_stats.svg\`"
