b060875207c4e8898a057f5422c05c533238effe
Roan Rothrock <roan.rothrock@hotmail.com>
2023-07-21 08:10:14 -0500

    Updated stats

--- update_github_stats.sh
+++ update_github_stats.sh
@@ -4,13 +4,10 @@ update_apis_github(){
   git init
   git pull
 
-  rm "activity_stats.svg"
-  curl "https://github-readme-stats.vercel.app/api?username=R-Rothrock&show_icons=true&theme=react" > "activity_stats.svg"
-  git add "activity_stats.svg"
-
   rm "language_stats.svg"
   curl "https://github-readme-stats-sigma-five.vercel.app/api/top-langs/?username=R-Rothrock&theme=react&line_height=40" > "language_stats.svg"
-  git add "language_stats.svg"
+
+  git add *
 
   git commit -m "Updated stats"
   git push
